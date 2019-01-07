3e9a81af292c2a293201777dfd2184595cdbd130
michael <zdong2010@gmail.com>
2019-01-07 13:53:04 +0800

    'page测试'

---
+++ pages/posts/_id.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h1 class="display-1">Post {{ $route.params.id }}</h1>
+  </div>
+</template>
---
+++ pages/posts/hello.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container">
+    <h1 class="display-1">Hello</h1>
+  </div>
+</template>
